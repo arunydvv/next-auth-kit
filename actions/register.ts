d577941852c91f3fd5c9095657e4ecf85bcd9237
arunydvv <arunyadav9704@gmail.com>
2024-12-30 10:21:30 +0530

    Setting up Register Action

--- actions/register.ts
+++ actions/register.ts
@@ -1,44 +1,41 @@
-"use server"
+"use server";
 
-import { db } from "@/lib/db"
-import bcrypt from "bcrypt"
-import { z } from "zod"
-import { RegisterSchema } from "@/schemas"
+import { db } from "@/lib/db";
+import bcrypt from "bcrypt";
+import { z } from "zod";
+import { RegisterSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
 
+export const register = async (
+  values: z.infer<typeof RegisterSchema>
+): Promise<{ error?: string; success?: string }> => {
+  const validatedFields = RegisterSchema.safeParse(values);
 
-// we are using server actions here
-
-export const register = async (values: z.infer<typeof RegisterSchema>) => {
-    const validatedFields = RegisterSchema.safeParse(values)
-
-    if (!validatedFields.success) {
-        return {
-            error: "Invalid Fields!"
-        }
-    }
-    const { email, password, name } = validatedFields.data;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const existingUser = await db.user.findUnique({
-        where: {
-            email: email
-        }
-    });
-
-    if (existingUser) {
-        return {
-            error: "User already exists!"
-        }
-    }
-
-    const newUser = await db.user.create({
-        data: {
-            email,
-            password: hashedPassword,
-            name
-        }
-    });
-
+  if (!validatedFields.success) {
     return {
-        success : "User Created"
-    }
-}
+      error: "Invalid Fields!",
+    };
+  }
+
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return {
+      error: "User already exists!",
+    };
+  }
+
+  const newUser = await db.user.create({
+    data: {
+      email,
+      password: hashedPassword,
+      name,
+    },
+  });
+
+  return {
+    success: "User Created",
+  };
+};
