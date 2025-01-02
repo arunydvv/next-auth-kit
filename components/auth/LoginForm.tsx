ccba12c9d9143a4f6c7bed210de2f6209a307c17
arunydvv <arunyadav9704@gmail.com>
2025-01-02 17:02:21 +0530

    Setting up Credentials Provider

--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -14,6 +14,20 @@ import { FormSuccess } from '../ui/FormSuccess'
 import { login } from '@/actions/login'
 
 
+import { signIn } from "@/auth"
+
+export  function SignIn() {
+    return (
+        <form
+            action={async () => {
+                "use server"
+                await signIn("github")
+            }}
+        >
+            <button type="submit">Signin with GitHub</button>
+        </form>
+    )
+} 
 
 
 const LoginForm = () => {
